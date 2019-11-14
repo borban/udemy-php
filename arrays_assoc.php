f26a95d291687b487014bff2635b4b0151f3d919
Ben Orban <benjamin.orban@ehi.com>
2019-11-14 08:48:45 -0600

    Lesson 13

---
+++ arrays_assoc.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php
+
+$number = ["juanita", "maria", "jose"];
+
+//echo $number[2] . "<br>";
+
+//print_r($number);
+echo "<br>";
+$names = ["first_name" => "Benjamin", "last_name" => "Orban"];
+
+//print_r($names);
+
+echo $names["first_name"] . " " . $names["last_name"];
+
+?>
+</body>
+</html>
