7c49a2a2dfc36f624d717533a74dcd20d93c1fe2
Ben Orban <benjamin.orban@gmail.com>
2020-01-05 09:38:50 -0600

    Lesson 35

--- form.php
+++ form.php
@@ -1,11 +1,28 @@
 <?php
 
 if(isset($_POST['submit'])){
+
+    $name = array("Edwin", "Student", "Benjamin", "Samid", "Mohad", "Maria", "Jane", "tom");
+    $minimum = 5;
+    $maximum = 10;
+
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    echo "Hello " . $username . "<br>";
-    echo "Your password is" . $password;
+    if(strlen($username) < $minimum){
+        echo "Username has to be longer than five ";
+    }
+
+    if(strlen($username) > $maximum){
+        echo "Username cannot be longer than 10 ";
+    }
+
+    if(!in_array($username, $name)){
+        echo "Sorry, you are not allowed";
+    }
+    else{
+        echo "Welcome!";
+    }
 
 }
 
